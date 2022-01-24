493f0696153baaf396d575597fc342f7afebe918
amite <amite@DESKTOP-EDIG2VH>
2022-01-24 12:03:26 +0530

    Add project files.

---
+++ WebApi/Controllers/NewApiController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    public class NewApiController : ApiController
+    {
+        practiceEntities1 db = new practiceEntities1();
+
+        [System.Web.Http.HttpGet]
+        public IHttpActionResult Index()
+        {
+            List<Models.student> obj = db.students.ToList();
+            return Ok(obj);
+        }
+
+        [System.Web.Http.HttpGet]
+        public IHttpActionResult Index(int id)
+        {
+            var obj = db.students.Where(model => model.id == id).FirstOrDefault();
+            return Ok(obj);
+        }
+    }
+}
